d263685d46f2f33028cdc5210ef9de805ff24dfa
Andrea Solano <yasolanoc@udistrital.edu.co>
2026-02-03 20:49:44 -0500

    Gob

--- program.js
+++ program.js
@@ -3,3 +3,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([4.651795, -74.09462]).addTo(map);
