4be78ed146d40e85fb1d326d400b15110e1ed0df
Andrea Solano <yasolanoc@udistrital.edu.co>
2026-02-03 20:46:55 -0500

    t

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.651795, -74.09462], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
